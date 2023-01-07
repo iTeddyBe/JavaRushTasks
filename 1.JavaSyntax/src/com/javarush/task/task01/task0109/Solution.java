ea29f8693301b1efb9db47a51c4358791b579cdb
TeddyBe <sevmon@mail.ru>
2023-01-07 13:56:59 +0300

    task0109

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 12;
+        int b = 14;
+    }
+}
---
+++ Test/src/lesson01/FirstClass.java
@@ -0,0 +1,8 @@
+package lesson01;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
