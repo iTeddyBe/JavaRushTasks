8c1567c2e6096ba833511613c87edb20e8e3e1b8
TeddyBe <sevmon@mail.ru>
2023-01-08 05:23:06 +0300

    task0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int intValue = 10;
+        int numericValue = -20;
+        int size = 100;
+        int number = -5;
+    }
+}
