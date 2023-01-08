af478bc87efec510d918e4c9d3143c29f4b1990f
TeddyBe <sevmon@mail.ru>
2023-01-08 14:04:12 +0300

    task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String word = "word";
+        String phrase = "phrase";
+        String line = "line";
+        String text = "text";
+
+    }
+}
