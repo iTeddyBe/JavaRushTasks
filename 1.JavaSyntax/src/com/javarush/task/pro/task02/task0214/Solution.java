ad49e363fb546dd5b3334fc93a0e72076853cbed
TeddyBe <sevmon@mail.ru>
2023-01-09 03:52:24 +0300

    Commit by hand 2 - task 0214

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -10,6 +10,14 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner keyboard_data = new Scanner(System.in);
+        String line1 = keyboard_data.nextLine();
+        String line2 = keyboard_data.nextLine();
+        String line3 = keyboard_data.nextLine();
+
+        System.out.println(line3);
+        System.out.println(line2.toUpperCase());
+        System.out.println(line1.toLowerCase());
 
     }
 }
