f585ec7e3c384e2d2933d73f5239f1216092f5e8
TeddyBe <sevmon@mail.ru>
2023-01-09 03:51:17 +0300

    commit by hand - task 0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
