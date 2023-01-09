64c7ac76025fa0f610e97c1a289e95766e983fe4
TeddyBe <sevmon@mail.ru>
2023-01-09 04:52:25 +0300

    change name

---
+++ z.MyExamples/src/Example01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Example01 {
+
+
+    public static void main(String[] args) {
+        System.out.println("out text");
+
+        Scanner keyboard_in = new Scanner("text");
+
+        System.out.println(keyboard_in.nextLine());
+    }
+
+}
