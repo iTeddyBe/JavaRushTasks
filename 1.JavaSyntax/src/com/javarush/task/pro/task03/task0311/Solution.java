089ae063e4f56718ab887819d01d5565caead9ca
TeddyBe <sevmon@mail.ru>
2023-01-13 01:48:56 +0300

    task 0308

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double a = console.nextDouble();
+        double b = console.nextDouble();
+        
+        if (Math.abs(a-b) < 0.000001) {
+            System.out.println("числа равны");
+        } else {
+            System.out.println("числа не равны");
+        }
+    }
+}
