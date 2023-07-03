e3dc3fefa7bd27e2ab3afc49d873df6ecc91cdbb
TeddyBe <sevmon@mail.ru>
2023-07-03 03:21:55 +0300

    update 03.07.2023a

--- 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -7,6 +7,7 @@ package com.javarush.task.task01.task0142;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        System.out.println("Амиго очень умный1");
+
+        System.out.println("Амиго очень умный");
     }
 }
